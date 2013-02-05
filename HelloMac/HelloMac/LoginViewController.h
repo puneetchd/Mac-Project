4aeae6e454e584694ed6797cb196434060a9c54e
Puneet Sharma <puneet94174@yahoo.co.in>
2013-02-05 14:44:54 +0530

    Login Controller

---
+++ HelloMac/HelloMac/LoginViewController.h
@@ -0,0 +1,13 @@
+//
+//  LoginViewController.h
+//  HelloMac
+//
+//  Created by Puneet's on 05/02/13.
+//  Copyright (c) 2013 Puneet's. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface LoginViewController : NSViewController
+
+@end
---
+++ HelloMac/HelloMac/LoginViewController.m
@@ -0,0 +1,27 @@
+//
+//  LoginViewController.m
+//  HelloMac
+//
+//  Created by Puneet's on 05/02/13.
+//  Copyright (c) 2013 Puneet's. All rights reserved.
+//
+
+#import "LoginViewController.h"
+
+@interface LoginViewController ()
+
+@end
+
+@implementation LoginViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Initialization code here.
+    }
+    
+    return self;
+}
+
+@end
