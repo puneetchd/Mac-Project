304148f3cc70b8a87d094fed94b1f428f92aa541
Puneet Sharma <puneet94174@yahoo.co.in>
2013-02-05 14:42:00 +0530

    Mac Project Starting
    Testing

---
+++ HelloMac/HelloMac/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  HelloMac
+//
+//  Created by Puneet's on 05/02/13.
+//  Copyright (c) 2013 Puneet's. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+@property (assign) IBOutlet NSWindow *window;
+
+@end
---
+++ HelloMac/HelloMac/AppDelegate.m
@@ -0,0 +1,25 @@
+//
+//  AppDelegate.m
+//  HelloMac
+//
+//  Created by Puneet's on 05/02/13.
+//  Copyright (c) 2013 Puneet's. All rights reserved.
+//
+
+#import "AppDelegate.h"
+
+@implementation AppDelegate
+
+- (void)dealloc
+{
+    [super dealloc];
+}
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification
+{
+    // Insert code here to initialize your application
+    
+    NSLog(@"Start Application");
+}
+
+@end
